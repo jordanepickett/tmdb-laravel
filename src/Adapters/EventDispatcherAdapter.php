3c7f5b22769438ba19fd8e541183b6e1b6ebc3b0
jordan_pickett <jordan@ninjacat.io>
2020-11-09 00:22:22 -0700

    string

--- src/Adapters/EventDispatcherAdapter.php
+++ src/Adapters/EventDispatcherAdapter.php
@@ -109,7 +109,7 @@ abstract class EventDispatcherAdapter implements SymfonyDispatcher
      *
      * @return array The event listeners for the specified event, or all event listeners by event name
      */
-    public function getListeners(string $eventName = null)
+    public function getListeners($eventName = null)
     {
         return $this->symfonyDispatcher->getListeners($eventName);
     }
@@ -121,7 +121,7 @@ abstract class EventDispatcherAdapter implements SymfonyDispatcher
      *
      * @return bool true if the specified event has any listeners, false otherwise
      */
-    public function hasListeners(string $eventName = null)
+    public function hasListeners($eventName = null)
     {
         return ($this->symfonyDispatcher->hasListeners($eventName) ||
             $this->laravelDispatcher->hasListeners($eventName));
